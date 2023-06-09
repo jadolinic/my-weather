6479a7605bdba906cf6c87d8e8886e587908e206
jadolinic <jadolinic@gmail.com>
2023-06-09 11:11:26 +0200

    styling weather app

--- src/App.js
+++ src/App.js
@@ -10,53 +10,58 @@ function App() {
   const [search, setSearch] = useState("");
   const [weather, setWeather] = useState({});
 
-  const searchPressed = () => {
-    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-      .then((data) => data.json())
-      .then((data) => {
-        setWeather(data);
-      });
+  const searchPressed = (e) => {
+    if (e.keyCode === 13) {
+      fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+        .then((data) => data.json())
+        .then((data) => {
+          setWeather(data);
+        });
+    }
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* Header */}
-
-        <h1>My weather App</h1>
-
-        {/* Search field */}
-
-        <div>
-          <input
-            type="text"
-            placeholder="search city"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button onClick={searchPressed}>Search</button>
-        </div>
-
-        {/*Fixing error 404 (page not found)*/}
-        
-        {typeof weather.main != "undefined" ? (
-          <div>
-            {/* Location */}
-
-            <p>{weather.name}</p>
-
-            {/* Temperature */}
-
-            <p>{weather.main.temp}°C</p>
-
-            {/* Weather Conditions */}
-
-            <p>{weather.weather[0].main}</p>
-            <p>({weather.weather[0].description})</p>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search city"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          onKeyDown={searchPressed}
+        />
+      </div>
+      {typeof weather.main != "undefined" ? (
+        <header className="container">
+          <div className="top">
+            <div className="location">
+              <p>{weather.name}</p>
+            </div>
+            <div className="temperature">
+              <h1>{weather.main.temp.toFixed()}°C</h1>
+            </div>
+            <div className="description">
+              <p>{weather.weather[0].main}</p>
+            </div>
           </div>
-        ) : 
-          ""
-        }
-      </header>
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">{weather.main.feels_like.toFixed()}°C</p>
+              <p>Feels like</p>
+            </div>
+            <div className="humidity">
+              <p className="bold">{weather.main.humidity}%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p className="bold">{weather.wind.speed.toFixed()}m/s</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </header>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
